9a20e698963bf008e1e57dacd77c60773b195cec
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-03-05 22:51:00 -0500

    Update 7-1.r

--- Chapter 07/Question 7.1/7-1.r
+++ Chapter 07/Question 7.1/7-1.r
@@ -4,7 +4,11 @@ library(lattice)
 library(ggplot2)
 
 
+# create a data Grid
+dataGrid<-data.frame(x=seq(2,10,length=100))
 
+# this is done to divide the graph in 4 columns
+par(mfrow = c(2,2))
 
   
   
