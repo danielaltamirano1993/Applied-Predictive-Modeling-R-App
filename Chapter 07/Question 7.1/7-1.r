fb42b3b961aa9e108b4a80165298c1a547ddc20b
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-03-05 23:01:46 -0500

    Update 7-1.r

--- Chapter 07/Question 7.1/7-1.r
+++ Chapter 07/Question 7.1/7-1.r
@@ -10,6 +10,8 @@ dataGrid<-data.frame(x=seq(2,10,length=100))
 # this is done to divide the graph in 4 columns
 par(mfrow = c(2,2))
 
+                 kernel="rbfdot",kpar="automatic",
+                 C= svmParam1$costs[i],epsilon = svmParam1$eps[i])
   
   
   
