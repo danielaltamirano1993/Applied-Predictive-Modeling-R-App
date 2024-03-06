657611cc85517c7083c7526fa34a0f85c8604c12
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-03-05 23:01:52 -0500

    Update 7-1.r

--- Chapter 07/Question 7.1/7-1.r
+++ Chapter 07/Question 7.1/7-1.r
@@ -3,6 +3,9 @@ library(kernlab)
 library(lattice)
 library(ggplot2)
 
+x<-runif(100,min=2,max=10)
+y<-sin(x)+rnorm(length(x))*0.25
+sinData<-data.frame(x=x,y=y)
 
 # create a data Grid
 dataGrid<-data.frame(x=seq(2,10,length=100))
