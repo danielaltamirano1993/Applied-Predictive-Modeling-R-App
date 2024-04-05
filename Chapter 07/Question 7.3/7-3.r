b3e14e98b0cd59ac13e9c70a1bd485e93ecfe1eb
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-04-04 21:17:19 -0500

    Update 7-3.r

--- Chapter 07/Question 7.3/7-3.r
+++ Chapter 07/Question 7.3/7-3.r
@@ -76,6 +76,8 @@ nnetGrid1 <- expand.grid(.decay = c(0, 0.01, .1),
                          ## The next option is to use bagging (see the
                          ## next chapter) instead of different random
                          ## seeds.
+                         .bag = FALSE)
+
 
 
 
