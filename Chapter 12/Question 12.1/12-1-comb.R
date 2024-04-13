d73e31e5f184a36eb5547cdc259313c0e157b64b
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-04-13 10:06:31 -0500

    Update 12-1-comb.R

--- Chapter 12/Question 12.1/12-1-comb.R
+++ Chapter 12/Question 12.1/12-1-comb.R
@@ -1,11 +1,14 @@
 library(caret)
 library(AppliedPredictiveModeling)
 
+data(hepatic)
 # use ?hepatic to see more details
 
 
 library(MASS)
+set.seed(975)
 
+#this gives Z114 predictor has zero-variance
 
 
 
