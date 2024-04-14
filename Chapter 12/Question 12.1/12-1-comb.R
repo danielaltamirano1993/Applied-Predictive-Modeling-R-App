a993cc141f21de6a076b69b81d7bfd2316cd5682
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-04-14 10:06:48 -0500

    Update 12-1-comb.R

--- Chapter 12/Question 12.1/12-1-comb.R
+++ Chapter 12/Question 12.1/12-1-comb.R
@@ -9,6 +9,10 @@ library(MASS)
 set.seed(975)
 
 #this gives Z114 predictor has zero-variance
+nearZeroVar(bio)
+
+#remove the Z114 predictor and then find the correlation between the predictors
+noZVbio <- bio[,-114]
 
 
 
